df18b40451b679ea4f971e4ddae0560fd9d9092f
Vishwadeep Bhosale <75164928+vishbhosale007@users.noreply.github.com>
2023-10-31 10:56:09 +0530

    Added Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.header('My parents new healthy diner')
+streamlit.title('My parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
